950518430d6efb343e99395c806ab4296361a83b
carlosEA28 <carloseaduarte28@gmail.com>
2025-03-31 20:55:13 -0300

    creation of pedidos and itemsPedido entity

---
+++ pedido/pedido/src/main/java/com/microservicos/pedido/entity/ItemPedidoEntity.java
@@ -0,0 +1,56 @@
+package com.microservicos.pedido.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemPedidoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String nome;
+    private Integer quantidade;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private PedidoEntity pedidoEntity;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public PedidoEntity getPedidoEntity() {
+        return pedidoEntity;
+    }
+
+    public void setPedidoEntity(PedidoEntity pedidoEntity) {
+        this.pedidoEntity = pedidoEntity;
+    }
+}
---
+++ pedido/pedido/src/main/java/com/microservicos/pedido/entity/PedidoEntity.java
@@ -0,0 +1,47 @@
+package com.microservicos.pedido.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class PedidoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID pedidoId;
+
+    private String descricao;
+
+    @OneToMany
+    private List<ItemPedidoEntity> itens;
+
+    public UUID getPedidoId() {
+        return pedidoId;
+    }
+
+    public void setPedidoId(UUID pedidoId) {
+        this.pedidoId = pedidoId;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<ItemPedidoEntity> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemPedidoEntity> itens) {
+        this.itens = itens;
+    }
+}
